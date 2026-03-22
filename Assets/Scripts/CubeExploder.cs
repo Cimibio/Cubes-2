7dbb53a258dc2971878bf6130e7bac32b6c9fd8a
Cimibio <shingaryovan@gmail.com>
2026-03-22 13:35:10 +1200

    2 commit
    added calculate to force by distance

--- Assets/Scripts/CubeExploder.cs
+++ Assets/Scripts/CubeExploder.cs
@@ -5,8 +5,21 @@ public class CubeExploder : MonoBehaviour
 {
     public void ExplodeCubes(Cube cube)
     {
-        foreach (Rigidbody affectedObjects in GetAffectedObjects(cube))
-            affectedObjects.AddExplosionForce(cube.ExplodeForce, cube.transform.position, cube.ExplodeRadius);
+        foreach (Rigidbody affectedObject in GetAffectedObjects(cube))
+        {
+            float distance = Vector3.Distance(affectedObject.position, cube.transform.position);
+            float forceMultiplier = CalculateForceMultiplier(distance, cube.ExplodeRadius);
+            float finalForce = cube.ExplodeForce * forceMultiplier;
+
+            affectedObject.AddExplosionForce(finalForce, cube.transform.position, cube.ExplodeRadius);
+        }
+    }
+
+    private float CalculateForceMultiplier(float distance, float radius)
+    {
+        if (distance >= radius) return 0f;
+
+        return 1f - (distance / radius);
     }
 
     private List<Rigidbody> GetAffectedObjects(Cube cube)
